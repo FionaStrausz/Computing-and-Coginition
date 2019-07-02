d68ce9c089ff259781a991a1f0bc796c4be85f4e
FionaStrausz <52449987+FionaStrausz@users.noreply.github.com>
2019-07-02 12:12:37 +0200

    Verison 8
    More patterns and responses, and a way were the swap pronoun work better and don't repeats itselfs.

--- Echobot.py
+++ Echobot.py
@@ -12,40 +12,45 @@ responses = {"hi":["Hello","Goodday","Hi"],
              "what are you doing":["Nothing... Why?","I am eating koekjes, and you?","Right now I am answering your question","I am solving a cube in my mind"],
              "why": ["Because I like it", "Doesn't matter", "What why?", "Because I tell you so"],
              "how old are you":["Infinity","I don't age","That is none of your business"],
-             "how do you feel":["Awesome","Better than yesterday, and how do you feel?","I feel tired, and you?"],
-             "what should I do":["Meyby you should get some more sleep","I think you should go outside more","Meyby you should drink some water"]} 
+             "how do you feel":["Awesome","Better than yesterday, and how do you feel?","I feel tired, and you?"]} 
 
-pattern_responses = {"do you remember (.*)": ["Of course I remember {}", "No I can't remember {}"],
-                     "i feel (.*)": ["Why do you feel {}", "You feel {}, why?"],
-                    "can i sit (.*)": ["Of course you can sit {}", "No you are not allowed to sit {}", "I don't know whether you can sit {} or not"],
-                    "do you (.*)": ["No I don't {}", "I don't know if I {}", "Yes, I do {}"],
-                    "if (.*)": ["{} already"],
-                    "i am in (.*)": ["How is it in {}", "I heard {} is annoying", "I don't like to be in {}"],
+pattern_responses = {"do you remember (.*)": ["Of course I remember{}", "No I can't remember {} I'm very sorry"],
+                     "i feel (.*)": ["I understand, why do you feel {}", "You feel {}, why?"],
+                    "can i sit (.*)": ["Of course you can sit{}", "No you are not allowed to sit{}", "I don't know whether you can sit{}or not"],
+                    "do you (.*)": ["No I don't{}", "I don't know if I{}", "Yes, I do{}"],
+                    "did you (.*)": ["No I didn't{}", "I don't know if I{}", "Yes, I did{}"],
+                    "if (.*)": ["{}"],
+                    "i am in (.*)": ["How is it in{}", "I heard{}is annoying, but how do you feel about it", "I don't like to be in{}, how do you feel about it?"],
                     "why (.*)": ["Because I like it", "Doesn't matter", "What why?", "Because I tell you so"],
                     "(.*)weather(.*)":["It is very very bad","Once in 2003 it was freezing.","It is going to rain someday"],
                     "(.*)happy(.*)":["I am glad that you feel happy","Did something happen?","Well that is good"],
-                    "(.*)bad(.*)":["I am sorry that you feel sad","Can I help you with something?","That is too bad"],
+                    "(.*)bad(.*)":["I am sorry that you feel bad","Can I help you with something?","That is too bad"],
                     "(.*)i don't know(.*)":["That is okay too","Alright","Can I help you with something"],
-                    "can you help me(.*)":["Of course can I help you{}","Yes I can help you{} please tell me with wath","I would love to help you{}"],
-                    "my name is(.*)":["{} is a nice name","I wish my name was{}","Hello{} my name is ALIE"],
-                    "(.*)talk(.*)":["Tell me about it","What's up","Okay, begin"]}
+                    "can you help me(.*)":["Of course can I help you{}","Yes I can help you{}please tell me with wath","I would love to help you{}"],
+                    "my name is(.*)":["{}is a nice name","I wish my name was{}","Hello{}my name is ALIE"],
+                    "(.*)talk(.*)":["Tell me about it","What's up","Okey, begin"],
+                    "(.*)what should i do":["Maybe you should get some more sleep","I think you should go outside more","Maybe you should drink some water", "A friend of mine meets with friends"]}
 
 
 def swap_pronoun(phrase):
+  phrase = ' ' + phrase + ' '
+  
   if ' me ' in phrase:
-    return re.sub('me','you', phrase)
- # elif ' you 'in phrase:
-    #return re.sub('you', 'I', phrase)
-  elif ' I ' in phrase:
-    return re.sub('I', 'you', phrase)
-  elif ' am ' in phrase:
-    return re.sub('am', 'are', phrase)
-  elif ' my ' in phrase:
-    return re.sub('my','your', phrase)
-  elif ' your ' in phrase:
-    return re.sub('your','mine', phrase)
-  else:
-    return phrase
+    phrase = re.sub(' me ',' |you ', phrase)
+  if ' i ' in phrase:
+    phrase = re.sub(' i ', ' |you ', phrase)
+  if ' am ' in phrase:
+    phrase = re.sub(' am ', ' |are ', phrase)
+  if ' my ' in phrase:
+    phrase = re.sub(' my ', ' |your ', phrase)
+  if ' you 'in phrase: 
+    phrase = re.sub(' you', ' |I ', phrase)
+  if ' your 'in phrase: 
+    phrase = re.sub(' your ', ' |my ', phrase)
+  if ' are 'in phrase: 
+    phrase = re.sub(' are ', ' |am ', phrase)
+  phrase = phrase.replace("|","")
+  return phrase
 
 def check_pattern(message):
   for pattern in pattern_responses:
@@ -57,12 +62,14 @@ def check_pattern(message):
 def respond(message):
   message = message.lower()
   message = message.replace("?", "")
+  message = message.replace("!", "")
+  message = message.replace(".", "")
   if message in responses:
     return random.choice(responses[message])
   elif check_pattern(message):
     return "{}".format(check_pattern(message))
   else: 
-    return random.choice(["Hmmm", "Anyway", "That is very interesting","I don't understand"])
+    return random.choice(["Hmmm", "Okey", "That is very interesting","I don't understand"])
 
 def send_message(message):
   print("You: {}".format(message))
@@ -71,6 +78,3 @@ def send_message(message):
   
 while True:
   send_message(input())
-
-
-
