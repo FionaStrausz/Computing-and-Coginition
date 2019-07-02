9ea8ae37ab2bd27eada1f124f576c1d8687d43b9
FionaStrausz <52449987+FionaStrausz@users.noreply.github.com>
2019-07-02 12:10:04 +0200

    Version 5
    More patterns and responses.

--- Echobot.py
+++ Echobot.py
@@ -1,21 +1,30 @@
 import time,random,re
 
-responses = {"Hi":["Hello","Goodday","Hi"],
-             "Hello":["Hello","Goodday","Hi"],
-             "Goodday":["Hello","Goodday","Hi"],
-             "What is your name?":["My name is ALIE","ALIE","They call me ALIE"],
-             "How are you?":["Fine","Worse than you","I am doing great"],
-             "Koekjes":["Koekjes", "Awesome", "I like koekjes"]} 
 
-pattern_responses = {"Do you remember (.*)": ["Of course I remember {}", "No I can't remember {}"],
-                     "I feel (.*)": ["Why do you feel {}", "You feel {}, why?"]}
+
+responses = {"hi":["Hello","Goodday","Hi"],
+             "hello":["Hello","Goodday","Hi"],
+             "goodday":["Hello","Goodday","Hi"],
+             "what is your name?":["My name is ALIE","ALIE","They call me ALIE"],
+             "how are you?":["Fine","Worse than you","I am doing great"],
+             "koekjes":["Koekjes", "Awesome", "I like koekjes"],
+             "where do you live?":["Nowhere", "I am not shure", "That is a good question", "I rather not talk about it"]} 
+
+pattern_responses = {"do you remember (.*)": ["Of course I remember {}", "No I can't remember {}"],
+                     "i feel (.*)": ["Why do you feel {}", "You feel {}, why?"],
+                    "can i sit (.*)": ["Of course you can sit {}", "No you are not allowed to sit {}", "I don't know whether you can sit {} or not"],
+                    "do you (.*)": ["No I don't {}", "I don't know if I {}", "Yes, I do {}"],
+                    "if (.*)": ["{} already"],
+                    "i am in (.*)": ["How is it in {}", "I heard {} is annoying", "I don't like to be in {}"]}
 
 
 def swap_pronoun(phrase):
-  if 'me' in phrase:
+  if ' me' in phrase:
     return re.sub('me','you', phrase)
-  elif 'you'in phrase:
+  elif ' you'in phrase:
     return re.sub('you', 'I', phrase)
+  elif ' I' in phrase:
+    return re.sub('I', 'you', phrase)
   else:
     return phrase
 
@@ -27,6 +36,7 @@ def check_pattern(message):
       return answer.format(swap_pronoun(match.group(1)))
 
 def respond(message):
+  message = message.lower()
   if message in responses:
     return random.choice(responses[message])
   elif check_pattern(message):
@@ -41,4 +51,5 @@ def send_message(message):
   
 while True:
   send_message(input())
+
 
