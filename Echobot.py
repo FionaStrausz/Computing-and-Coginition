2b4576e42cad38c4b6c1cc3515552f54a64b9afd
FionaStrausz <52449987+FionaStrausz@users.noreply.github.com>
2019-07-02 12:10:46 +0200

    Version 6
    More patterns and responses and swap pronoun words.

--- Echobot.py
+++ Echobot.py
@@ -5,17 +5,21 @@ import time,random,re
 responses = {"hi":["Hello","Goodday","Hi"],
              "hello":["Hello","Goodday","Hi"],
              "goodday":["Hello","Goodday","Hi"],
-             "what is your name?":["My name is ALIE","ALIE","They call me ALIE"],
-             "how are you?":["Fine","Worse than you","I am doing great"],
+             "what is your name":["My name is ALIE","ALIE","They call me ALIE"],
+             "how are you":["Fine how do you feel?","Worse than you","I am doing great"],
              "koekjes":["Koekjes", "Awesome", "I like koekjes"],
-             "where do you live?":["Nowhere", "I am not shure", "That is a good question", "I rather not talk about it"]} 
+             "where do you live":["Nowhere", "I am not shure", "That is a good question", "I rather not talk about it"],
+             "what are you doing":["Nothing, Why?","I am eating cookies","Right now I am answering your question","I am solving a cube in my mind"],
+             "why": ["Because I like it", "Doesn't matter", "What why?", "Because I tell you so"]} 
 
 pattern_responses = {"do you remember (.*)": ["Of course I remember {}", "No I can't remember {}"],
                      "i feel (.*)": ["Why do you feel {}", "You feel {}, why?"],
                     "can i sit (.*)": ["Of course you can sit {}", "No you are not allowed to sit {}", "I don't know whether you can sit {} or not"],
                     "do you (.*)": ["No I don't {}", "I don't know if I {}", "Yes, I do {}"],
                     "if (.*)": ["{} already"],
-                    "i am in (.*)": ["How is it in {}", "I heard {} is annoying", "I don't like to be in {}"]}
+                    "i am in (.*)": ["How is it in {}", "I heard {} is annoying", "I don't like to be in {}"],
+                    "why (.*)": ["Because I like", "Doesn't matter", "What why?", "Because I tell you so"],
+                    "(.*)weather(.*)":["It is very bad","Once in 2003 it was freezing.","It is going to rain someday"]}
 
 
 def swap_pronoun(phrase):
@@ -25,6 +29,8 @@ def swap_pronoun(phrase):
     return re.sub('you', 'I', phrase)
   elif ' I' in phrase:
     return re.sub('I', 'you', phrase)
+  elif ' am' in phrase:
+    return re.sub('am', 'are', phrase)
   else:
     return phrase
 
@@ -37,6 +43,7 @@ def check_pattern(message):
 
 def respond(message):
   message = message.lower()
+  message = message.replace("?", "")
   if message in responses:
     return random.choice(responses[message])
   elif check_pattern(message):
