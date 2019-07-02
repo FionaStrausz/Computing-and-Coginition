b6bc12b07698ed01b9df7509ec00c3d9330f2c76
FionaStrausz <52449987+FionaStrausz@users.noreply.github.com>
2019-07-02 12:08:09 +0200

    Version 3
    The Echobot is able to recognize patterns.

--- Echobot.py
+++ Echobot.py
@@ -4,19 +4,33 @@ responses = {"Hi":["Hello","Goodday","Hi"],
              "Hello":["Hello","Goodday","Hi"],
              "Goodday":["Hello","Goodday","Hi"],
              "What is your name?":["My name is ALIE","ALIE","They call me ALIE"],
-             "How are you?":["Fine","Worse than you","I am doing great"]} 
+             "How are you?":["Fine","Worse than you","I am doing great"],
+             "Koekjes":["Koekjes", "Awesome", "I like koekjes"]} 
+
+pattern_responses = {"Do you remember (.*)": ["Of course I remember {}", "No I can't remember {}"],
+                     "I feel (.*)": ["Why do you feel {}", "You feel {}, why?"]}
+
+
+
+def check_pattern(message):
+  pattern = "Do you remember (.*)"
+  match = re.search(pattern, message)
+  if match:
+    answer = random.choice(pattern_responses[pattern])
+    return answer.format(match.group(1))
 
 def respond(message):
   if message in responses:
     return random.choice(responses[message])
+  elif check_pattern(message):
+    return "{}".format(check_pattern(message))
   else: 
-    return"I don't know what to respond to: {}".format(message)
+    return random.choice(["Hmmm", "Anyway", "That is very interisting", "Whatever"])
 
 def send_message(message):
-  print("USER: {}".format(message))
-  time.sleep(random.randint(1,3))
-  print("ECHOBOT: {}".format(respond(message)))
+  print("You: {}".format(message))
+  time.sleep(random.randint(0,3))
+  print("ALIE: {}".format(respond(message)))
   
 while True:
   send_message(input())
-
